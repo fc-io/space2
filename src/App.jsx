789a0c8bc63463a94c883e9a9a04b40c7a32aa12
Fredrik Carlsson <me@fc.io>
2023-02-24 09:45:32 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import './App.css'
+
+// const fetchFromApi = () => {
+//   return
+// }
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const query = useQuery({
+    queryKey: ['page'],
+    queryFn: () => {
+      return fetch('https://api.spaceflightnewsapi.net/v3/articles')
+        .then((response) => response.json())
+        .then((data) => {
+          return data
+        })
+    }
+  })
+  // console.log(query)
+
+  if (query.isLoading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1>Space News</h1>
+      {query.data.map(({ title }) => <div style={{marginTop: '1rem'}}>{title}</div>)}
+    </div>
+  )
+}
+
+export default App
